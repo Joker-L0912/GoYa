c342fd3f0f108f9e8d01029e5812333ab1f7a1eb
Limou <1522282814@qq.com>
2023-11-09 23:25:53 +0800

    更新和新增时自动修改字段

--- goya-issue/goya-issue-model/src/main/java/com/goya/issue/model/po/BaseModel.java
+++ goya-issue/goya-issue-model/src/main/java/com/goya/issue/model/po/BaseModel.java
@@ -1,11 +1,14 @@
 package com.goya.issue.model.po;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
@@ -27,22 +30,27 @@ public class BaseModel {
      */
     @CreatedDate
     @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+    @Column(name = "created_at", columnDefinition = "datetime(3)")
     private Date createdAt;
 
     /**
      * updated_at
      */
-    @LastModifiedBy
+    @LastModifiedDate
+    @CreatedDate
     @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+    @Column(name = "update_at", columnDefinition = "datetime(3)")
     private Date updatedAt;
 
     /**
      * created_by
      */
+    @CreatedBy
     private String createdBy;
 
     /**
      * updated_by
      */
+    @LastModifiedBy
     private String updatedBy;
 }
---
+++ goya-issue/goya-issue-service/src/main/java/com/goya/issue/service/config/UserAuditor.java
@@ -0,0 +1,19 @@
+package com.goya.issue.service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+
+/**
+ * @author limoum0u
+ * @date 23/11/9 23:01
+ */
+@Configuration
+public class UserAuditor implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("limoum0u");
+    }
+}
